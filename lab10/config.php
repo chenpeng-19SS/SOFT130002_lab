bde7a7aa0123b44816fa16398b7cc609177fc31d
chenpeng-19SS <19302010028@fudan.edu.cn>
2020-06-08 00:09:24 +0800

    lab10: 补交

---
+++ lab10/config.php
@@ -0,0 +1,5 @@
+<?php define('DBHOST', 'localhost');
+define('DBNAME', 'art');
+define('DBUSER', '19302010028');
+define('DBPASS', '816520cp');
+define('DBCONNSTRING','mysql:dbname=art;charset=utf8mb4;'); ?>
