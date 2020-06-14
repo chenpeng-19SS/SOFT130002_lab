fc64823b35d228d5926e6b947e5d7e566a1e86a9
chenpeng-19SS <61626112+chenpeng-19SS@users.noreply.github.com>
2020-06-14 22:42:45 +0800

    Add files via upload

---
+++ lab11/config.php
@@ -0,0 +1,7 @@
+<?php
+define('DBHOST', 'localhost');
+define('DBNAME', 'Login');
+define('DBUSER', 'root');
+define('DBPASS', '20011002');
+define('DBCONNSTRING','mysql:host=localhost;dbname=Login');
+?>
